0b1754a69843de7ac74b05d736308001a7f18844
Umaimazainab02 <umaimazainab429@gmail.com>
2026-08-01 16:38:47 +0500

    Added loop practice questions

--- app.js
+++ app.js
@@ -8,9 +8,22 @@ sun1.addEventListener("click", function () {
     moon1.style.display = "inline-block";
     document.body.style.backgroundColor = "black";
     document.body.classList.add("text-white");
-      document.getElementById("colorchng").style.background =
-        "linear-gradient(to right, #8C5607 0%, #5C340D 50%, #8E571F 100%)"; 
-        document.getElementById("textcolor").style.color = "white";
+    document.getElementById("colorchng").style.background =
+        "linear-gradient(to right, #8C5607 0%, #5C340D 50%, #8E571F 100%)";
+    document.getElementById("textcolor").style.color = "white";
+    document.getElementById("project1").style.background = "#18181B";
+    document.getElementById("project2").style.background = "#18181B";
+    document.getElementById("project3").style.background = "#18181B";
+    document.getElementById("project4").style.background = "#18181B";
+    document.getElementById("project5").style.background = "#18181B";
+    document.getElementById("project6").style.background = "#18181B";
+        document.getElementById("name1").style.color = "white";
+        document.getElementById("name2").style.color = "white";
+        document.getElementById("name3").style.color = "white";
+        document.getElementById("name4").style.color = "white";
+        document.getElementById("name5").style.color = "white";
+        document.getElementById("name6").style.color = "white";
+
 });
 
 moon1.addEventListener("click", function () {
@@ -19,6 +32,19 @@ moon1.addEventListener("click", function () {
     document.body.style.backgroundColor = "white";
     document.body.classList.remove("text-white");
     document.getElementById("colorchng").style.background =
-    "linear-gradient(980deg, #FEDDA0 0%, #F9F7DB 45%, #F9F7DB 70%, #F7DA70 100%)";
+        "linear-gradient(980deg, #FEDDA0 0%, #F9F7DB 45%, #F9F7DB 70%, #F7DA70 100%)";
     document.getElementById("textcolor").style.color = "gray";
+    document.getElementById("project1").style.background = "white";
+    document.getElementById("project2").style.background = "white";
+    document.getElementById("project3").style.background = "white";
+    document.getElementById("project4").style.background = "white";
+    document.getElementById("project5").style.background = "white";
+    document.getElementById("project6").style.background = "white";
+        document.getElementById("name1").style.color = "black";
+        document.getElementById("name2").style.color = "black";
+        document.getElementById("name3").style.color = "black";
+        document.getElementById("name4").style.color = "black";
+        document.getElementById("name5").style.color = "black";
+        document.getElementById("name6").style.color = "black";
+
 })
